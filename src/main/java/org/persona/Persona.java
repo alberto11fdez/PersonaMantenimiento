272b6b8151bd6b31e9512fa0a6503ba8a75a0509
alberto11fdez <alberto11fdez@uma.es>
2023-02-09 12:13:35 +0100

    Clase Persona Hecha

--- src/main/java/org/persona/Persona.java
+++ src/main/java/org/persona/Persona.java
@@ -22,7 +22,13 @@ public class Persona {
 
     public Persona(String name, int age, String gender){
         this.name = name;
-        this.age = age;
+
+        if(age < 0 || age > 150){
+            throw new AgeNegativeException("No puedes no haber nacido o ser un muerto viviente");
+        }else{
+            this.age = age;
+        }
+
         if(gender.toUpperCase() != "Male" || gender.toUpperCase() != "Female"){
             throw new NoGenderException("No existe ese genero");
         }else{
@@ -49,7 +55,26 @@ public class Persona {
      */
 
     public double[] averageAgePerGender(List<Persona> persons){
-        return null;
+
+        int contMale=0;
+        int contFemale=0;
+        int ageMale=0;
+        int ageFemale=0;
+
+        double [] average = null;
+
+        for (int i = 0; i < persons.size(); i++){
+            if(persons.get(i).gender == "Male"){
+                ageMale += persons.get(i).age;
+                contMale++;
+            }else if(persons.get(i).gender == "Female"){
+                ageFemale += persons.get(i).age;
+                contFemale++;
+            }
+        }
+        average[0] = ageMale/contMale;
+        average[1] = ageFemale/contFemale;
+        return average;
     }
 
 }
